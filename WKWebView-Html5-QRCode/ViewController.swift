d51fbb8a14eec0136fa3838c0844974dafd9b55d
Simon Pickup <simon@pickupinfinity.com>
2021-01-25 21:14:58 +0000

    Initial Commit

---
+++ WKWebView-Html5-QRCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WKWebView-Html5-QRCode
+//
+//  Created by Simon Pickup on 25/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
